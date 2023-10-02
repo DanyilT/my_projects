d0e7adfcd160cffd4b7a596f35c7f1471d2c93af
Danyil Tymchuk <71641218+DanyilT@users.noreply.github.com>
2023-10-02 09:36:57 +0100

    Delete test directory

--- test/src/main/java/org/dany_inc/telegram_bot/domain/Position.java
+++
@@ -1,7 +0,0 @@
-package org.dany_inc.telegram_bot.domain;
-
-public enum Position {
-    INPUT_USERNAME,
-    INPUT_PHONE_NUMBER,
-    NONE
-}
--- test/src/main/java/org/dany_inc/telegram_bot/handlers/Handler.java
+++
@@ -1,5 +0,0 @@
-package org.dany_inc.telegram_bot.handlers;
-
-public interface Handler<T> {
-    void  choose(T t);
-}
